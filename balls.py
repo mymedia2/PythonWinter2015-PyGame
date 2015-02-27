26bd49bd07d4d556f2853ed4cbf83e4866173ede
Guriev Nicholas <guriev-ns@ya.ru>
2015-02-27 22:24:01 +0300

    Исправлен баг при нажатии по пустому экрану. Fixed

--- balls.py
+++ balls.py
@@ -140,7 +140,8 @@ class GameWithDnD(GameWithObjects):
                     self.drag.pos = event.pos
                     self.drag.speed = event.rel
         elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-            self.drag.active = True
+            if self.drag is not None:
+                self.drag.active = True
             self.drag = None
         GameWithObjects.Events(self, event)
 
